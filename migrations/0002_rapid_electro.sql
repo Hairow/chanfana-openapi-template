0f7be866de03fd489f2fbf8d893326d2fb2ff57a
hairow <hairow@foxmail.com>
2026-07-21 13:37:15 +0800

    feat(tasks): 添加任务状态字段及校验描述

---
+++ migrations/0002_rapid_electro.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `tasks` ADD `status` integer DEFAULT 0 NOT NULL;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,6 +6,7 @@ import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 export const tasks = sqliteTable("tasks", {
 	id: integer("id").primaryKey({ autoIncrement: true }),
 	name: text("name").notNull(),
+	status: integer("status").notNull().default(0),
 	slug: text("slug").notNull().unique(),
 	description: text("description").notNull().default(''),
 	// integer({ mode: "boolean" }) 自动处理 SQLite 0/1 ↔ JS true/false
