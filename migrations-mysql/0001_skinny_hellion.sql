a6176137c6b46559081fad6366e20415c59064f5
hairow <hairow@foxmail.com>
2026-07-22 14:38:03 +0800

    fix(schema): 修复时间戳字段并添加软删除

---
+++ migrations-mysql/0001_skinny_hellion.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `users` MODIFY COLUMN `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
+ALTER TABLE `users` MODIFY COLUMN `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;--> statement-breakpoint
+ALTER TABLE `users` ADD `deleted_at` timestamp;
--- src/db-mysql/schema.ts
+++ src/db-mysql/schema.ts
@@ -5,6 +5,7 @@ export const users = mysqlTable("users", {
     id: int("id").primaryKey().autoincrement(),
     name: varchar("name", { length: 255 }).notNull().unique(),
     email: varchar("email", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-    updateAt: timestamp("update_at").default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+    updateAt: timestamp("update_at").notNull().default(sql`CURRENT_TIMESTAMP`).onUpdateNow(),
+    deletedAt: timestamp("deleted_at"),
 });
